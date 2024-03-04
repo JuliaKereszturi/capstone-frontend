71299280a7b4d8332564fe1702c24c12c7090cb7
Anastasia <z.anastasia11@gmail.com>
2024-03-04 14:10:52 -0800

    add personal details component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import "./App.css";
 import { ReactComponent as CalenderSVG } from "./assets/icons/time/calender-1.svg";
+import PersonalDetails from "./components/PersonalProfile/PersonalDetails";
 function App() {
   return (
-    <div className="App flex mt-12 border-8 w-1/2 bg-primary-200 ">
-      <h1 className="mr-2 pr-4 border-r-4 font-roboto">COMING SOON PAGE</h1>
-      <CalenderSVG className="w-10 h-10 stroke-current fill-current text-primary-300" />
-    </div>
+    // <div className="App flex mt-12 border-8 w-1/2 bg-primary-200 ">
+    //   <h1 className="mr-2 pr-4 border-r-4 font-roboto">COMING SOON PAGE</h1>
+    //   <CalenderSVG className="w-10 h-10 stroke-current fill-current text-primary-300" />
+    //   <PersonalDetails/>
+    // </div>
+<div className=" m-12  ">
+      <PersonalDetails />
+</div>
+   
   );
 }
 
