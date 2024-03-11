eb413d08918a9d6c8050ac8861a6aea09d3ebb45
Squarehead2 <118778307+Squarehead2@users.noreply.github.com>
2024-03-11 12:45:54 -0600

    changed typography to be globally accessible, also changed character spacing to be smaller

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import "./App.css";
-import ContractorSignUp from "./pages/new-accounts/organizationSignUp";
-import OrganizationSignUp from "./pages/new-accounts/contractorSignUp";
+import OrganizationSignUp from "./pages/new-accounts/organizationSignUp";
 function App() {
-  return <OrganizationSignUp />;
+  return (
+    <div>
+      <OrganizationSignUp />
+    </div>
+  );
 }
 
 export default App;
