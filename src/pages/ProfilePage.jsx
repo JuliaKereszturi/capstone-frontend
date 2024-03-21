0a86a460dbf6dd798515af1e08068f0661009b2f
Julia Kereszturi <jkeresz1@asu.edu>
2024-03-21 15:10:10 -0700

    added resume upload card to profile

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,17 +1,24 @@
 import React from "react";
 import ProfileTabs from "../components/ProfileTabs";
-import Sidebar from "../components/Sidebar";
 import ProfileForm from "../components/ProfileForm";
-import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
+import ResumeUploadCard from "../components/PersonalProfile/ResumeUploadCard";
 
 export default function ProfilePage() {
   return (
     <>
       <ProfileTabs />
 
-      <h1 className="text-lg font-medium">Personal Details</h1>
+      <div className="flex flex-row ">
+        <div className="flex-auto">
+          <h1 className="text-lg font-medium">Personal Details</h1>
 
-      <ProfileForm />
+          <ProfileForm />
+        </div>
+
+        <div className="max-w-[450px]">
+          <ResumeUploadCard />
+        </div>
+      </div>
     </>
   );
 }
